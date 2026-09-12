7714fad482213b73e78fc4e04ee4dbdaafc27377
LawkCornieur <lawk.cornieur@gmail.com>
2026-09-12 02:43:36 +0200

    Konťa (Beta): fix: oprava GetDirectory chyby při otevření nastavení v hlavním menu (#42)

--- plugin.video.streamcontinuum-beta/addon.py
+++ plugin.video.streamcontinuum-beta/addon.py
@@ -131,12 +131,8 @@ def list_categories():
             list_item = xbmcgui.ListItem(label=display_label)
             list_item.setArt({'icon': icon, 'thumb': icon, 'fanart': fanart})
             
-            if action == 'settings':
-                url = f"RunPlugin({sys.argv[0]}?action=settings)"
-                is_folder = False
-            else:
-                url = f"{sys.argv[0]}?action={action}"
-                is_folder = True
+            url = f"{sys.argv[0]}?action={action}"
+            is_folder = False if action == 'settings' else True
                 
             success = xbmcplugin.addDirectoryItem(HANDLE, url, list_item, isFolder=is_folder)
             if success:
@@ -2415,8 +2411,12 @@ def run():
         trakt_search()
     elif action == 'settings':
         ADDON.setSetting('about_version', ADDON.getAddonInfo('version'))
+        if HANDLE >= 0:
+            try:
+                xbmcplugin.endOfDirectory(HANDLE, succeeded=False)
+            except Exception:
+                pass
         ADDON.openSettings()
-        xbmc.executebuiltin('Container.Refresh')
         return
     elif action == 'search':
         search(params.get('query'))
